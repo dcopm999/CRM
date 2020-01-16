9f151cf1e07f2741b2ebe3c0137c56087de3e8bc
Pavel Tanchev <dcopm999@gmail.com>
2020-01-16 19:11:06 +0500

    add contragent and contact models to admin panel

--- contragents/admin.py
+++ contragents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from contragents import models
+
+@admin.register(models.Contragent)
+class ContragentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'inn', 'address_ur']
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'middle_name', 'contragent']
