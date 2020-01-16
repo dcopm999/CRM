6f6084ef2b864fc2559dffb3ad8058eb10a4a4d7
Pavel Tanchev <dcopm999@gmail.com>
2020-01-16 19:10:31 +0500

    Add verbose name for contragents app

--- contragents/apps.py
+++ contragents/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class ContragentsConfig(AppConfig):
     name = 'contragents'
+    verbose_name = _('Contragents and contacts')
