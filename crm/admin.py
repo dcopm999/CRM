1c1161e38b2dd65a9d65368454bd8a8165047452
Pavel Tanchev <dcopm999@gmail.com>
2020-01-20 13:13:21 +0500

    Change models

--- crm/admin.py
+++ crm/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from crm.models import AgreementStatus, Agreement
+from crm.models import AgreementTask, TaskStatus, Agreement
 
-@admin.register(AgreementStatus)
-class AgreementStatusAdmin(admin.ModelAdmin):
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
     list_display = ['name', 'created', 'edited']
+
+
+@admin.register(AgreementTask)
+class AgreementTaskAdmin(admin.ModelAdmin):
+    list_display = ['status', 'created', 'edited']
+
 
 @admin.register(Agreement)
 class AgreementAdmin(admin.ModelAdmin):
-    list_display = ['code', 'status', 'result', 'created', 'edited']
+    list_display = ['code', 'result', 'created', 'edited']
     list_filter = ['code', ]
--- crm/models.py
+++ crm/models.py
@@ -3,9 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from uuid import uuid4
 
+from contragents.models import Contact
 
-class AgreementStatus(models.Model):
-    name = models.CharField(max_length=50, verbose_name=_('Agreement status'))
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=50, verbose_name=_('Task status'))
     created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created date"), editable=False)
     edited = models.DateTimeField(auto_now=True, verbose_name=_("Edited date"), editable=False)
 
@@ -13,8 +15,8 @@ class AgreementStatus(models.Model):
         return self.name
     
     class Meta:
-        verbose_name = _('Agreement status')
-        verbose_name_plural = _('Agreement statuses')
+        verbose_name = _('Task status')
+        verbose_name_plural = _('Task statuses')
 
 
 class Agreement(models.Model):
@@ -24,7 +26,6 @@ class Agreement(models.Model):
         (3, _('In progress'))
     ]
     code = models.UUIDField(default=uuid4, verbose_name=_('Unique agreement code'))
-    status = models.ForeignKey(AgreementStatus, on_delete=models.CASCADE, verbose_name=_('Agreement status'))
     result = models.PositiveSmallIntegerField(choices=RESULT, verbose_name=_('Agreement result'))
     created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created date"), editable=False)
     edited = models.DateTimeField(auto_now=True, verbose_name=_("Edited date"), editable=False)
@@ -36,3 +37,20 @@ class Agreement(models.Model):
         verbose_name = _('Agreement')
         verbose_name_plural = _('Agreements')
     
+
+class AgreementTask(models.Model):
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE, verbose_name=_('Agreement status'))
+    agreement = models.ForeignKey(Agreement, on_delete=models.CASCADE, verbose_name=_('Agreement'))
+    contact = models.ForeignKey(Contact, verbose_name=_("Contact"), on_delete=models.CASCADE)
+    desc = models.TextField(verbose_name=_('Description'))
+    date = models.DateTimeField(auto_now=True, verbose_name=_('Task date'))
+    is_active = models.BooleanField(default=False, verbose_name=_('Is sctive'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created date"), editable=False)
+    edited = models.DateTimeField(auto_now=True, verbose_name=_("Edited date"), editable=False)
+
+    def __str__(self):
+        return f'{self.agreement}'
+    
+    class Meta:
+        verbose_name = _('Task')
+        verbose_name_plural = _('Tasks')
